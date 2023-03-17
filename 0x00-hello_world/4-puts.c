b0348be9dad5b1f3696541a23865dd8347fe8217
Ernest SHAIBU <ernestshiabu@gmail.com>
2023-03-16 17:03:53 -0700

    task6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- *main - main entry point
- *
- *Return: Always 0
- */
+  *main - a line with puts function
+  *return: always 0
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
