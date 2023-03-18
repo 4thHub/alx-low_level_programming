156c6d5552f7c6344585b03893c9e2cf8452f399
Ernest SHAIBU <ernestshiabu@gmail.com>
2023-03-17 18:12:23 -0700

    Done with the mandatory phase of this project

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - printf the alphabets a-z and A-Z
+ *Return: Always 0
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
