16f57da01a823aa32cd3625dc6b2bf67000f1a2f
Ernest SHAIBU <ernestshiabu@gmail.com>
2023-03-21 10:57:00 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - point of entry
+ *Description - print "_putchar"
+ *Return: 0 always
+ */
+
+int main(void)
+{
+	write(1, "_putchar", strlen("_putchar"));
+
+	return (0);
+}
