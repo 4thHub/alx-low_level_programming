e139124490ee8927046da9f5fb12e2e179aedf6f
Ernest SHAIBU <ernestshiabu@gmail.com>
2023-04-03 20:07:29 -0700

    08 and 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *set_string - assign value
+ *@s: Pointer indicator
+ *@blow: Charactor indicator
+ *Return: void
+ */
+void set_string(char **s, char *blow)
+{
+	*s = blow;
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_diagsums -display sums of diagonals
+ *@a: Matrix of integers
+ *@size: the size
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		a += size;
+	}
+	a -= size;
+	for (i = 0; i < size; i++)
+	{
+		s2 += a[i];
+		a -= size;
+	}
+
+	printf("%d, %d\n", s1, s2);
+}
