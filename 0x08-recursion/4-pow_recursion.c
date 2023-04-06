14a45f85f3155298c076505df5df0d9bbcb43b45
Ernest SHAIBU <ernestshiabu@gmail.com>
2023-04-05 18:48:26 -0700

    task4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_pow_recursion - return the value of two umbers raised to the power
+ *@x: a number
+ *@y: another number
+ *Return: -1 || or the value of the digits raised to the power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
