43d24055f25fcd01219dab453d315f56c14f6094
Ernest SHAIBU <ernestshiabu@gmail.com>
2023-04-09 20:51:59 -0700

    task1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ *main - point of entry
+ *description: print the number of arguement passed int the programme
+ *@argc: The number of arguements
+ *@argv: The array of arguements
+ *Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
 	(void)argv;
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
