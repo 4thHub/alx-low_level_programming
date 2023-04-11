4eb076d969590b41db60499647afc8da66af023d
Ernest SHAIBU <ernestshiabu@gmail.com>
2023-04-11 16:17:24 -0700

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_strdup - returns pointer to newly allocated space
+ *@str: string inputed
+ *Return: NULL || ptr
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	int x = 0;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[x] != 0)
+		x++;
+	ptr = malloc((i + 1) * sizeof(chsr));
+	if (ptr == NULL)
+		return (NULL);
+	for (; str[i]; i++)
+		ptr[i] = str[i];
+	return (ptr);
+}
