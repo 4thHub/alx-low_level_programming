cf281a8c11354b3091a7f7949a90e13929dd0a37
Ernest SHAIBU <ernestshiabu@gmail.com>
2023-04-11 15:22:51 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,10 +14,8 @@ char *create_array(unsigned int size, char c)
 
 	ptr = malloc(size * sizeof(char));
 	if (ptr == NULL || size == 0)
-	{
-		return(NULL);
-	}
+		return (NULL);
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
-	return(ptr);
+	return (ptr);
 }
