e709690f7d3548d99979bdfca2befb5795bf2fd8
Ernest SHAIBU <ernestshiabu@gmail.com>
2023-04-19 16:28:26 -0700

    task1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *array_iterator - print function
+ *@array: array parameter in function
+ *@size: number of element
+ *@action: pointer to the function
+ *Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
