55ce6fd80285dd58df1fe54178c6a597a1f49ec2
Ernest SHAIBU <ernestshiabu@gmail.com>
2023-04-27 19:42:32 -0700

    task0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - a function that prints all the element of a linked list
+ * @h: the link to the list being printed
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		if (h == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
